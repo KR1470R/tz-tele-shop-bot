a7094213ee4428f49a40955f20f0573b7656c7f4
KR1470R <vartus2@gmail.com>
2021-01-06 23:23:32 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import Telegraf from "telegraf"
+
+console.log("token!", process.env.BOT_TOKEN)
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
