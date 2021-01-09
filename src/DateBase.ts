c1397e9227d619b660c1218716a3ad671a0ba3e3
KR1470R <vartus2@gmail.com>
2021-01-10 01:35:40 +0200

    finish

--- src/DateBase.ts
+++ src/DateBase.ts
@@ -1,27 +1,73 @@
 class DateBase {
+    /**
+     * basket: [{
+     *     username: [[product_name,  product_price], ...],
+     *     ...
+     * }]
+     *
+     * products: [[product_name,  product_price, amount], ...]
+     *
+     * orders : [{
+     *     username: id_order
+     * }]
+     *
+     * @private
+     */
     private storage = new Map([
         ["basket", [{}]],
-        ["products", []]
+        ["products", []],
+        ["orders", [{}]]
     ]);
-    public getter(type : string, name : number) {
-        return this.storage.get(type).filter(el => el[0] === name);
+    public getter(type : string, name : string) {
+        if (type === "products") {
+            return this.storage.get(type).filter(el => el[0] === name);
+        } else {
+            return this.storage.get(type)[0][name];
+        }
+    }
+    public setAmount(type : string, username : string, nameProduct : string, value : number) {
+        if (type === "basket") {
+            this.storage.get(type)[0][username].map(el => {
+                if (el[0] === nameProduct) {
+                    this.storage.get(type)[0][username][this.storage.get(type)[0][username].indexOf(el)][2] = value;
+                    console.log("DDBVALUE", value)
+                    console.log("DBBB2", this.storage.get(type)[0][username][this.storage.get(type)[0][username].indexOf(el)][2])
+                }
+            })
+        }
     }
     public getAll(type : string) {
         return this.storage.get(type);
     }
-    public adder(type : string, username : string, key : string, value : string) : void {
+    public adder(type : string, username : string, key : string, value : string, amount : number) : void {
         if (type === "basket") {
-            if (this.storage.get(type)[0][username]) {
-                this.storage.get(type)[0][username].push([key, value]);
+            if (db.getAll("basket")[0][username]) {
+                this.storage.get(type)[0][username].push([key, value, amount]);
             } else {
-                this.storage.get(type)[0][username] = [[key, value]];
+                this.storage.get(type)[0][username] = [[key, value, amount]];
             }
-        } else {
+        } else if (type === "products") {
             this.storage.get(type).push([key, value]);
+        } else if (type === "orders") {
+            this.storage.get(type)[0][username] = [new Date().valueOf()];
         }
     }
     public clearBasket(username : string) {
         this.storage.get("basket")[0][username] = [];
+    }
+    public countBasketContent(username : string) {
+        let array = db.getAll("basket")[0][username];
+        if (array) {
+            let counts = {};
+            for(let i = 0; i < array.length; i++){
+                if (counts[array[i]]){
+                    counts[array[i]] += 1;
+                } else {
+                    counts[array[i]] = 1;
+                }
+            }
+            return counts;
+        }
     }
 }
 export const db = new DateBase();
--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,11 @@ const startScene = patternScene.startScene();
 const adminPanel = patternScene.adminPanel();
 const addProduct = patternScene.addProduct();
 const goToBasket = patternScene.goToBasket();
-const stage = new Stage([startScene, adminPanel, addProduct, goToBasket]);
+const checkout = patternScene.checkout();
+const final = patternScene.final();
+const stage = new Stage([startScene, adminPanel, addProduct, goToBasket, checkout, final]);
+
+export const admin_username : string = process.env.TG_ADMIN_ID;
 
 bot.use(session());
 bot.use(stage.middleware());
@@ -28,18 +32,45 @@ bot.command("start",ctx => {
 });
 
 bot.action("addToBasket", async ctx => {
-	let name_product = ctx.callbackQuery.message.text.split('\n')[0].replace("Название: ", "");
-	let price_product = ctx.callbackQuery.message.text.split('\n')[1].replace("Цена: ", "");
-	db.adder("basket", ctx.from.username, name_product, price_product);
+	let product_name = ctx.callbackQuery.message.text.split('\n')[0].replace("Название: ", "");
+	let product_price = ctx.callbackQuery.message.text.split('\n')[1].replace("Цена: ", "");
+	let count = db.countBasketContent(ctx.from.username);
+	console.log(count)
+	if (count && Object.keys(count).join("|").includes(product_name)) {
+		console.log( Object.keys(count).join("|"), "<<>>", product_name)
+		let current;
+		for (let el = 0; el <= Object.keys(count).length - 1; el++) {
+			if (Object.keys(count)[el].includes(product_name)) {
+				current = parseInt(Object.keys(count)[el].split(",")[2]) + 1;
+			};
+		}
+		db.setAmount("basket", ctx.from.username, product_name, current);
+	} else {
+		db.adder("basket", ctx.from.username, product_name, product_price, 1);
+	}
+})
+bot.hears("🛠Админ панель🛠", async ctx => {
+	if (admin_username === ctx.from.username) {
+		// @ts-ignore
+		ctx.scene.enter("adminPanel");
+	}
 })
 bot.action("clearBasket", async ctx => {
 	db.clearBasket(ctx.from.username);
-	ctx.editMessageText("Корзина очищена")
+	ctx.editMessageText("Корзина очищена");
 	// @ts-ignore
 	ctx.scene.leave();
 })
 bot.action("goToBasket", async ctx => {
 	// @ts-ignore
 	ctx.scene.enter("goToBasket");
+})
+bot.action("checkout", async ctx => {
+	// @ts-ignore
+	ctx.scene.enter("checkout");
+})
+bot.action("final", async ctx => {
+	// @ts-ignore
+	ctx.scene.enter("final");
 })
 bot.launch();
